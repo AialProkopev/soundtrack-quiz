4b56d15c9444d2624a7ae7e4026bd07db36307bd
Ayal Prokopev <freezzforever@gmail.com>
2023-01-20 13:23:20 +0300

    init: install and set up eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const mode = process.env.NODE_ENV || "development";
-const devMode = mode === "development";
-const devtool = devMode ? "source-map" : undefined;
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
   mode,
   devtool,
-  entry: path.resolve(__dirname, "src", "index.js"),
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    filename: "index.[contenthash].js",
+    filename: 'index.[contenthash].js',
     clean: true,
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     open: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
     new MiniCssExtractPlugin({
-      filename: "index.[contenthash].css",
+      filename: 'index.[contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(c|sa|sc)ss$/i,
         use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
