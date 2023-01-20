f97c7829e56efcdd666e6907ace6a898920bef13
Ayal Prokopev <freezzforever@gmail.com>
2023-01-20 12:53:38 +0300

    init: configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const devMode = mode === "development";
+const devtool = devMode ? "source-map" : undefined;
+
+module.exports = {
+  mode,
+  devtool,
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    filename: "index.[contenthash].js",
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
